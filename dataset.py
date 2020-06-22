d4f5da671750143103f448b8c6e4bdda8a5e16dd
Kai Chan <kaichan1201@gmail.com>
2020-06-22 17:42:43 +0800

    add training code

--- dataset.py
+++ dataset.py
@@ -3,8 +3,9 @@ import torchvision
 from torch.utils.data import Dataset
 
 class CustomData(Dataset):
-    def __init__(self, mode):
+    def __init__(self, mode, dir_path):
         self.mode = mode
+        self.dir_path = dir_path
 
     def __len__(self):
         pass
--- train.py
+++ train.py
@@ -1,13 +1,46 @@
 import torch
 from tqdm import tqdm
 
-def train(model, train_loader, criterion, optimizer):
+def train(model, train_loader, criterion_cls, optimizer, device, writer, cur_epoch):
     model.train()
-    for idx, (data, gt) in tqdm(enumerate(train_loader)):
-        out = model(data)
+    pbar = tqdm(enumerate(train_loader), total=len(train_loader), leave=False, ncols=100,
+                desc='Epoch {}'.format(cur_epoch))
+    offset = (cur_epoch-1)*len(train_loader)
+    for idx, (data, gt) in pbar:
+        data, gt = data.to(device), gt.to(device)
 
-def validate(model, val_loader, criterion, optimizer):
-    pass
+        out = model(data)
+        loss = criterion_cls(out, gt)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        pbar.set_postfix({'loss':'{:.5f}'.format(loss.item())})
+        writer.add_scalar('train/loss', loss.item(), offset + idx)
+
+def validate(model, val_loader, device, writer, cur_epoch):
+    model.eval()
+    total_cnt = 0
+    total_correct_cnt = 0
+    pbar = tqdm(enumerate(val_loader), total=len(val_loader), leave=False, ncols=100,
+                desc='Val {}'.format(cur_epoch))
+    with torch.no_grad():
+        for idx, (data, gt) in pbar:
+            data, gt = data.to(device), gt.to(device)
+
+            out = model(data)
+            _, pred = torch.max(out, dim=1)
+
+            total_cnt += pred.size(0).item()
+            total_correct_cnt += (pred == gt).sum().item()
+
+    val_acc = total_correct_cnt / total_cnt
+
+    print('Epoch {} | Val Acc: {:.5f}'.format(cur_epoch, val_acc))
+    writer.add_scalar('val/acc', val_acc, cur_epoch)
+
+    return val_acc
 
 def infer(model, test_loader):
     pass
